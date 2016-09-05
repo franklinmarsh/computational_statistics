955063060610451521b891dd389479259e9a0439
Franklin Marsh <franklink8@gmail.com>
2016-09-05 16:56:52 -0700

    start first homework assignment

---
+++ hw1_3.R
@@ -0,0 +1,21 @@
+G.Prime <- function(x) {
+  #returns the value of the function G.prime evaluated at x.
+  #
+  # Args:
+  #   x: the value at which to evaluate the function
+  #
+  # Returns:
+  #   g'(x)  
+  return((1 + 1/x - log(x)) / ((1+x) ^ 2))
+}
+
+#Bisect <- function(initial_guess, test_function){
+  # Find the root of an input function via bisection.
+  #
+  # Args:
+  #    initial_guess: the value at which first 
+  #
+  # Returns:
+  # 
+  
+#}
---
+++ hw1_4.R
@@ -0,0 +1,35 @@
+G.1 <- function(x) {
+  # returns the value of the function $G_{1}$ in problem 4.
+  #
+  # Args:
+  #   x: the value at which to evaluate the function
+  #
+  # Returns:
+  #   $G_{1}(x)$
+  
+  return()
+}
+
+G.2 <- function(x) {
+  # returns the value of the function $G_{2}$ in problem 4.
+  #
+  # Args:
+  #   x: the value at which to evaluate the function
+  #
+  # Returns:
+  #   $G_{2}(x)$
+  
+  return()
+}
+
+G.3 <- function(x) {
+  # returns the value of the function $G_{3}$ in problem 4.
+  #
+  # Args:
+  #   x: the value at which to evaluate the function
+  #
+  # Returns:
+  #   $G_{3}(x)$
+  
+  return()
+}
