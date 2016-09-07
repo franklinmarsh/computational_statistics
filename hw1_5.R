5055da6b48ee6bb3387fa8c813fd70e20e4ffc3e
Franklin Marsh <franklink8@gmail.com>
2016-09-06 23:07:54 -0700

    work on problem 5

--- hw1_5.R
+++ hw1_5.R
@@ -1,4 +1,4 @@
-MCG <- function(x, a = 166425,c = 1013904223 ,m = 2^32) {
+MCG <- function(x, a = 1664255,c = 1013904223 ,m = 2^10) {
   # mixed congruential generator function
   #
   # Args:
@@ -30,8 +30,31 @@ IterRecord <- function(inputfunction, seed, n_iter) {
   results <- list(seed)
   
   for (i in 2:n_iter) {
-    results[i] <- inputfunction(results[i-1])
+    results[i] <- inputfunction(as.numeric(results[i-1]))
   }
   
   return(results)
+}
+
+rand_numbers = IterRecord(MCG, 2, 1000)
+xi <- rand_numbers[1:length(rand_numbers) - 1]
+xi_plusone <- rand_numbers[2:length(rand_numbers)]
+
+plot(xi, xi_plusone)
+
+MiddleSquare <- function(seed) {
+  # compute a 'random' sequence of four-digit numbers using the Middle Square method.
+  #
+  # Args:
+  #   seed: the intial four digit number
+  # 
+  # Returns:
+  #   rand: new quasi-random number
+  
+  options("scipen" = 100, "digits" = 4) #disable scientific notation so the character operation works.
+  seed_squared <- toString(seed^(2.0))
+  rand <- as.numeric(substr(seed_squared, 3,6)) #take the middle slice out of an 8 digit number and return to numeric 
+  options("scipen"= 8, "digits" = 4)
+  
+  return(rand)
 }
