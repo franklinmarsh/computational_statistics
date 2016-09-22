bb5b14d8a080c9c735c453e7e3ae05451a4319f2
Franklin Marsh <franklink8@gmail.com>
2016-09-22 11:59:19 -0700

    write FindSize function in hw3.R

--- hw3.R
+++ hw3.R
@@ -4,22 +4,26 @@ GenX0 <- function(m) {
 
 GenSwaps <- function(x0) {
   
-  out <- list() #initialize empty list to hold all possible swaps
+  out <- c() #initialize empty list to hold all possible swaps
+  i <- 0
   
-  for (swap in x0) {
-    
+  for (j in (1:length(x0))) {
+    i <- i + 1
+    for (k in (1:length(x0))) {
+      i < i + 1
+        out[i] <- replace(x0, c(j,k), x0[c(k,j)])  
+    }
   }
-  
+  return(out)
 }
 
 FindSize <- function(x) {
   
-  sizes <- list()
+  sizes <- c()
   
-  for (j in (0:x) {
-      sizes[i] <- x[i]*i
+  for (j in (1:length(x))) {
+      sizes[j] <- x[j]*j
   }
   
   return(sum(sizes))
-    
 }
