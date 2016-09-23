9f0f9c475d11c570cc29d14391b4c4c3cc7c9181
Franklin Marsh <franklink8@gmail.com>
2016-09-22 17:00:58 -0700

    write GenOneSwap function in hw3.R

--- hw3.R
+++ hw3.R
@@ -4,18 +4,32 @@ GenX0 <- function(m) {
 
 GenSwaps <- function(x0) {
   
-  out <- c() #initialize empty list to hold all possible swaps
+  out <- list() #initialize empty list to hold all possible swaps
   i <- 0
   
-  for (j in (1:length(x0))) {
+  for (j in x0) {
     i <- i + 1
     for (k in (1:length(x0))) {
       i < i + 1
-        out[i] <- replace(x0, c(j,k), x0[c(k,j)])  
+        out[[i]] <- replace(x0, c(j,k), x0[c(k,j)])  
     }
   }
   return(out)
 }
+
+GenOneSwap <- function(x0) {
+  
+  out <- x0 #initialize empty list to hold all possible swaps
+
+  j <- sample((1:length(x0)),1) #pull and a random index from x0
+  k <- sample((1:length(x0)),1) #pull another random index from x0
+  
+  while (identical(out, x0) == TRUE) { #if we generate an identical object, keep going
+      out <- replace(x0, x0[c(j,k)], x0[c(k,j)])  
+  }
+  return(out)
+}
+
 
 FindSize <- function(x) {
   
@@ -27,3 +41,6 @@ FindSize <- function(x) {
   
   return(sum(sizes))
 }
+
+FindSize(GenOneSwap(GenX0(4)))
+
