a9b6b8e750ea7fa565ea7af976c6a88249391035
Franklin Marsh <franklink8@gmail.com>
2016-09-30 14:38:45 -0700

    get simannealing working in lab_franklin.R

--- lab_franklin.R
+++ lab_franklin.R
@@ -1,5 +1,37 @@
-f <- function(x) {
+h <- function(x) {
   # the function that we will be maximizing
   return(sin(10*x) - x^(2))
 }
 
+
+
+Step <- function(h, h_range, tau, input) {
+  
+  e <- exp(1) #make e easy to write
+  
+  candidate <- runif(1,h_range[1], h_range[2])
+  prob <- e^((h(candidate) - h(input))/(tau))
+  
+  if (prob > 1) {
+    out <- candidate
+  }
+  
+  else {
+    out <- input
+  }
+  
+  return(out)
+}
+
+SimAnnealing <- function(h, h_range, tau_range,tau_delta) {
+  
+  path <- c(input <- runif(1,h_range[1], h_range[2]))
+  
+  tau_schedule <- seq(tau_range[1], tau_range[2], -tau_delta)
+  n_iters <- length(tau_schedule)
+  
+  for (i in (2:(n_iters + 1))) {
+    path[i] <- Step(h, h_range, tau_schedule[i-1],path[i-1])
+  }
+  return(path)
+}
